8ef8b661f9611baabc6abc9a2701471776d6b424
Jeremyobrien <jeremy.j.obrien1@gmail.com>
2026-02-04 18:27:34 -0500

    removed artifacts and settings

--- out/build/x64-Debug/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/Users/jerem/source/repos/InvoiceApplication
-# Build directory: C:/Users/jerem/source/repos/InvoiceApplication/out/build/x64-Debug
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("tests")
